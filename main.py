1000645f73c6a77cd1d59a290e7a829cba8c6054
Vinayak Goel <vinayak21113@iiitd.ac.in>
2024-03-11 18:13:41 +0530

    funcs

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from concurrent import futures
 import grpc
 import os
 
+leader=False
+
+def timeout():
+    pass
+def startelection():
+    pass
+
+def reqvote():
+    pass
+
+def resvote():
+    pass
+
+
+
 def run():
     n=int(input("Enter Node ID : "))
     try:
@@ -11,6 +26,13 @@ def run():
         f = open(path+f"logs_node_{n}.txt","a+")
         f1= open(path+"metadata.txt","a+")
         f2= open(path+"dump.txt","a+")
+        if leader:
+            command = input("Enter command")
+            comm = command.split(" ")
+            if comm[0]=="GET":
+                print("Get Operation")
+            elif comm[0]=="SET":
+                print("Set Operation")
     except FileExistsError:
         pass
 
