73cfc41a3a2857971005341c105dc1a4853f94d2
vinayak-77 <vinayak270603@gmail.com>
2024-03-13 01:46:18 +0530

    Not following the pseudo-code ?

---
+++ raftNode.py
@@ -0,0 +1,38 @@
+import logEntry
+
+class Node:
+  isLeader = False
+  nodeId = -1
+  ipAddr = ""
+  port = ""
+  currentTerm = 0
+  votedFor = None
+  log = []
+  commitLength = 0
+  currentRole = "Follower"
+  currentLeader = None
+  votesReceived = []
+  sentLength = []
+  ackedLength = []
+  lastTerm = 0
+  
+  def __init__(self,nodeId,ip,port):
+    self.nodeId = nodeId
+    self.ipAddr = ip
+    self.port = port
+    
+  def onCrashRecovery(self):
+    self.currentRole = "Follower"
+    self.currentLeader = None
+    self.votesReceived = []
+    self.sentLength = []
+    self.ackedLength = []
+    
+  def onElectionTimeout(self):
+    self.currentTerm+=1
+    self.currentRole = "Candidate"
+    self.votedFor = self.nodeId
+    self.votesReceived = [self.nodeId]
+
+    if(len(self.log) > 0):
+      self.lastTerm = self.log[len(self.log)-1].term
